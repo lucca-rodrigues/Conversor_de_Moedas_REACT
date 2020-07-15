f4fe72de974159b5e1963ef98d95e2eba95d83fe
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2020-07-15 20:32:31 -0300

    List Money

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {FaArrowRight} from 'react-icons/fa'
+import {Jumbotron, Button, Form, Col, Spinner, Alert, Modal} from 'react-bootstrap';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Conversor de Moedas
+      </h1>
+      <Alert variant="danger" show={false}>
+        Erro ao obter os dados da conversão.
+      </Alert>
+      <Jumbotron>
+        <Form>
+          <Form.Row>
+            <Col sm="3">
+              <Form.Control 
+                placeholder="0"
+                value={1}
+                required
+              />
+            </Col>
+            <Col sm="3">
+              <Form.Control as="select">
+
+              </Form.Control>
+            </Col>
+            <Col sm="1">
+              <FaArrowRight size={20}/>
+            </Col>
+            <Col sm="3">
+              <Form.Control as="select">
+
+              </Form.Control>
+            </Col>
+            <Col sm="2">
+              <Button variant="success" type="submit">
+                <Spinner animation="border" size="sm"/>
+                Converter
+              </Button>
+            </Col>
+          </Form.Row>
+        </Form>
+        <Modal show={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              Conversão
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Resultado da conversão...
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="success">
+              Nova conversão
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Jumbotron>
     </div>
+      
   );
 }
 
--- src/ListarMoedas.test.js
+++ src/ListarMoedas.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import List from './ListarMoedas';
 
 describe('Teste do componente de listagem de moedas', () => {
 
   it('deve renderizar o componente sem erros', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(<List />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
