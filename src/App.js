f94c70eb6c0db186daed623f5a57d690bcfc7c08
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2020-07-15 21:40:54 -0300

    validation Form

--- src/App.js
+++ src/App.js
@@ -2,8 +2,37 @@ import React, {useState} from 'react';
 import './App.css';
 import {FaArrowRight} from 'react-icons/fa'
 import {Jumbotron, Button, Form, Col, Spinner, Alert, Modal} from 'react-bootstrap';
+import ListarMoedas from './ListarMoedas';
 
 function App() {
+  const [valor, setValor] = useState('');
+  const [moedaDe, setMoedaDe] = useState('BRL');
+  const [moedaPara, setMoedaPara] = useState('USD');
+  const [loading, setLoading] = useState(false);
+  const [validationForm, setValidationForm] = useState(false);
+
+  function handleValue(e){
+    setValor(e.target.value.replace(/\D/g, ''));
+    // Replace é uma expressão regular que remove todos os dados não numéricos do Input e retora ele Vazio com /\D
+    // caso utilizado o /\d ele inverte e exclui todos os numeros
+  }
+
+  function handleMoedaDe(e){
+    setMoedaDe(e.target.value);
+  }
+
+  function handleMoedaPara(e){
+    setMoedaPara(e.target.value);
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    setLoading(true);
+    setValidationForm(true);
+    if(e.currentTarget.checkValidity() === true){
+      
+    }
+  }
   
   return (
     <div>
@@ -14,32 +43,40 @@ function App() {
         Erro ao obter os dados da conversão.
       </Alert>
       <Jumbotron>
-        <Form>
+        <Form onSubmit={handleSubmit} noValidate validated={validationForm}>
           <Form.Row>
             <Col sm="3">
               <Form.Control 
                 placeholder="0"
-                value={1}
+                value={valor}
+                onChange={handleValue}
                 required
               />
             </Col>
             <Col sm="3">
-              <Form.Control as="select">
-
+              <Form.Control as="select"
+                value={moedaDe}
+                onChange={handleMoedaDe}
+              >
+                <ListarMoedas />
               </Form.Control>
             </Col>
-            <Col sm="1">
+            <Col sm="1" className="text-center">
               <FaArrowRight size={20}/>
             </Col>
             <Col sm="3">
-              <Form.Control as="select">
-
+              <Form.Control as="select"
+                value={moedaPara}
+                onChange={handleMoedaPara}
+              >
+                <ListarMoedas />
               </Form.Control>
             </Col>
             <Col sm="2">
-              <Button variant="success" type="submit">
-                <Spinner animation="border" size="sm"/>
-                Converter
+              <Button variant="success" onClick={handleSubmit}>
+                {loading && loading === true ?(<Spinner animation="border" size="sm"/>):(<span>Converter</span>)}
+                
+                
               </Button>
             </Col>
           </Form.Row>
