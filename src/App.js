e4fea2331e3a905b2f56ad5a924513b9ca27f3d9
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2020-07-16 13:59:32 -0300

    convertion money

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import {FaArrowRight} from 'react-icons/fa'
 import {Jumbotron, Button, Form, Col, Spinner, Alert, Modal} from 'react-bootstrap';
 import ListarMoedas from './ListarMoedas';
+import axios from 'axios';
 
 function App() {
+  const FIXER_URL = 'http://data.fixer.io/api/latest?access_key=eba7130a5b2d720ce43eb5fcddd47cc3';
+
   const [valor, setValor] = useState('');
   const [moedaDe, setMoedaDe] = useState('BRL');
   const [moedaPara, setMoedaPara] = useState('USD');
   const [loading, setLoading] = useState(false);
   const [validationForm, setValidationForm] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [resultConvertion, setResultConvertion] = useState('');
 
   function handleValue(e){
     setValor(e.target.value.replace(/\D/g, ''));
@@ -25,14 +30,40 @@ function App() {
     setMoedaPara(e.target.value);
   }
 
+  function CloseModal(e){
+    setValor('');
+    setMoedaDe('BRL');
+    setMoedaPara('USD');
+    setValidationForm(false);
+    setShowModal(false);
+    setLoading(false);
+  }
+
   function handleSubmit(e){
     e.preventDefault();
     setLoading(true);
     setValidationForm(true);
     if(e.currentTarget.checkValidity() === true){
-      
+     setLoading(true);
+      axios.get(FIXER_URL)
+        .then(response => {
+          const Coute = ResultConvertionValue(response.data)
+          setResultConvertion(`${valor} ${moedaDe} = ${Coute} ${moedaPara}`);
+          setShowModal(true);
+          setLoading(false);
+        })
     }
+    function ResultConvertionValue(result){
+      if(!result || result.success !== true){
+        return false;
+      }
+      const resultDe = result.rates[moedaDe];
+      const resultPara = result.rates[moedaPara];
+      const Coute = (1 / resultDe * resultPara) * valor;
+      return Coute.toFixed(2);
+    } 
   }
+
   
   return (
     <div>
@@ -75,23 +106,21 @@ function App() {
             <Col sm="2">
               <Button variant="success" onClick={handleSubmit}>
                 {loading && loading === true ?(<Spinner animation="border" size="sm"/>):(<span>Converter</span>)}
-                
-                
               </Button>
             </Col>
           </Form.Row>
         </Form>
-        <Modal show={false}>
+        <Modal show={showModal} onHide={CloseModal}>
           <Modal.Header closeButton>
             <Modal.Title>
               Conversão
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            Resultado da conversão...
+            {resultConvertion}
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="success">
+            <Button variant="success" onClick={CloseModal}>
               Nova conversão
             </Button>
           </Modal.Footer>
